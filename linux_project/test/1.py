fbe70e2b342b103194cccb95c7866f7d48c70a2e
liris <lirit@lirits.com>
2020-12-03 00:33:33 +0800

    Update 1.py

--- linux_project/test/1.py
+++ linux_project/test/1.py
@@ -2,37 +2,17 @@ import socket
 import cv2
 import pickle
 import struct
+import threading
 
 fourcc = cv2.VideoWriter_fourcc(*'XVID')
 out = cv2.VideoWriter('/home/pi/Desktop/output.avi', fourcc, 30, (640, 480))
-def Tcp_connect( host_ip, port):
-    socket_address = (host_ip, port)
-    global server_socket
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
-    server_socket.bind(socket_address)
-    server_socket.listen(5)
-
 def Main():
     while True:
-        client_socket, addr = server_socket.accept()
-        print('GOT CONNECTION FROM:', addr)
         if client_socket:
             vid = cv2.VideoCapture(0)
-            frame_id = 0
             while (vid.isOpened()):
                 img, frame = vid.read()
                 a = pickle.dumps(frame)
-                # frame_width = 640
-                # frame_height = 480
-                # frame_id += 1
-                # left_x_up = int(frame_width / frame_id)
-                # left_y_up = int(frame_height / frame_id)
-                # # 文字坐标
-                # word_x = left_x_up + 5
-                # word_y = left_y_up + 25
-                # name = 'opencv monitor'
-                # cv2.putText(frame, '%s' % name, (word_x, word_y), cv2.FONT_HERSHEY_SIMPLEX, 1, (55, 255, 155), 2)
                 out.write(frame)
                 message = struct.pack("Q", len(a)) + a
                 client_socket.sendall(message)
@@ -41,8 +21,15 @@ def Main():
                     client_socket.close()
 if __name__ == '__main__':
     try:
-        Tcp_connect('', 9999)
-        Main()
+        server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
+        server_socket.bind(('',9999))
+        server_socket.listen(5)
+        while True:
+            client_socket, addr = server_socket.accept()
+            print('GOT CONNECTION FROM:', addr)
+            test = threading.Thread(target=Main())
+        test.start()
     except ConnectionResetError:
         print('连接中断')
 
